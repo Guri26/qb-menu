dc3957665579d9c3f799b0fed9bf300d9f36d481
Guri26 <110395213+Guri26@users.noreply.github.com>
2023-06-17 21:52:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Guri'
+description 'qb-menu edit css'
+version '1.2.0'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
